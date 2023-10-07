f169012cdda45150002caeadb7a2694ce66f0667
Trebias <unitsss11@gmail.com>
2023-10-07 23:02:43 +0700

    fix: Починить тесты с запросами

--- EnglishApp/src/test/java/com/example/EnglishApp/EnglishAppApplicationTests.java
+++ EnglishApp/src/test/java/com/example/EnglishApp/EnglishAppApplicationTests.java
@@ -1,13 +1,36 @@
 package com.example.EnglishApp;
 
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
-class EnglishAppApplicationTests {
+import static org.assertj.core.api.Assertions.assertThat;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class CashCardApplicationTests {
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Test
+    void shouldReturnACashCardWhenDataIsSaved() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/LK/Name", String.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+		DocumentContext documentContext = JsonPath.parse(response.getBody());
+		Number id = documentContext.read("$.date");
+		assertThat(id).isNotNull();
+    }
 	@Test
-	void contextLoads() {
-	}
+	void shouldNotReturnACashCardWithAnUnknownId() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/LK/1000", String.class);
 
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+		assertThat(response.getBody()).isBlank();
+	}
 }
--- EnglishApp/src/test/java/com/example/EnglishApp/LKTest.java
+++ EnglishApp/src/test/java/com/example/EnglishApp/LKTest.java
@@ -17,12 +17,26 @@ public class LKTest {
     @Test
     public void cashCardSerializationTest() throws IOException {
         UserLK cashCard = new UserLK("Name", 10, 20);
-        //assertThat(json.write(cashCard)).isStrictlyEqualToJson("expected.json");
+        //assertThat(json.write(cashCard)).isStrictlyEqualToJson( "C:\\Users\\Cake\\EnglishApp\\EnglishApp\\src\\test\\java\\com\\example\\EnglishApp\\expected.json");
         assertThat(json.write(cashCard)).hasJsonPathStringValue("@.nick");
         assertThat(json.write(cashCard)).extractingJsonPathStringValue("@.nick")
                 .isEqualTo("Name");
         assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.date");
         assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.date")
              .isEqualTo(10);
+    }
+    @Test
+    public void cashCardDeserializationTest() throws IOException {
+        String expected = """
+                {
+                    "nick":"Name",
+                    "date":10,
+                    "learntWords":20
+                }
+                """;
+        assertThat(json.parse(expected))
+                .isEqualTo(new UserLK("Name", 10, 20));
+        assertThat(json.parseObject(expected).date()).isEqualTo(10);
+        assertThat(json.parseObject(expected).learntWords()).isEqualTo(20);
     }
 }
